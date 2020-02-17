020bcbd45f25915e5362e0bf75622d4cad302f8e
Paulo Marcelo <paulomarceloa.nogueira@gmail.com>
2020-02-17 00:37:53 -0300

    Change ManifestJson

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,12 +41,12 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `Paulo Marcelo Dev`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000`,
+        theme_color: `#000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/avatar/avatar.jpeg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
